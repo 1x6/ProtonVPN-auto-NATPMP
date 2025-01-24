da6f03e073e2c866c48fda73cdbefa7f185c5f47
1x6 <44981148+1x6@users.noreply.github.com>
2025-01-24 22:52:04 +0000

    massive fix

--- natpmpc_script_qbittorrent.sh
+++ natpmpc_script_qbittorrent.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
-# Path to the qbittorrent-nox.service file
-service_file="/etc/systemd/system/qbittorrent-nox.service"
+
+service_file="/etc/systemd/system/multi-user.target.wants/qbittorrent-nox@user.service"
+service="qbittorrent-nox@user"
+api_base_url="http://localhost:8080/api/v2/app"
+wireguard_interface="wireguardconfig"
+wireguard_config="/home/user/wireguardconfig.conf"
+
+sudo wg-quick up $wireguard_config
 while true; do
     date
 
-    # Run natpmpc for UDP and TCP, redirect output to a temporary file
-    natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > /tmp/natpmpc_output || { 
+    if sudo wg show | grep -q $wireguard_interface; then
+        echo "Wireguard interface is up"
+    else
+        echo "Wireguard interface not found. Restarting Wireguard..."
+        sudo wg-quick down $wireguard_config
+        sudo wg-quick up $wireguard_config
+    fi
+
+    natpmpc -a 1 0 udp 60 -g 10.2.0.1 > /tmp/natpmpc_output && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 > /tmp/natpmpc_output || { 
         echo -e "ERROR with natpmpc command \a" 
         break
     }
 
-    # Extract the port numbers from the output and save them in variables
     port=$(grep 'TCP' /tmp/natpmpc_output | grep -o 'Mapped public port [0-9]*' | awk '{print $4}')
-
     echo "Opened port: $port"
-    # Get the current --torrenting-port from the service file
-    current_port=$(grep -Po --max-count=1 '(?<=--torrenting-port=)[0-9]+' "$service_file")
 
-    # Check if $current_port is different from $port
-    if [ "$current_port" != "$port" ]; then
-        # Stop the qbittorrent-nox service
-        service qbittorrent-nox stop
-        echo "Current port is different. Changing from $current_port to $port"
-        # Update the service file with the new port
-        sed -i "s/--torrenting-port=$current_port/--torrenting-port=$port/" "$service_file"
-        # Reoload services
-        systemctl daemon-reload 
-        # Start the qbittorrent-nox service with the new port
-        service qbittorrent-nox start
+    old_port=$(curl -s -X GET "$api_base_url/preferences" | grep -o '"listen_port":[0-9]*' | grep -o '[0-9]*')
+    echo "The listening port is: $old_port"
+
+    current_network_interface=$(curl -s -X GET "$api_base_url/preferences" | grep -o '"current_network_interface": *"[^"]*"' | awk -F ':"' '{print $2}' | tr -d '"')
+    echo "Current network interface: $current_network_interface"
+
+    if [ "$current_network_interface" != "$wireguard_interface" ]; then
+        echo "Current network interface is different. Changing to $wireguard_interface"
+        curl -i -X POST -d "json={\"current_network_interface\": \"$wireguard_interface\"}" "$api_base_url/setPreferences"
+        echo "Sent request to change network interface"
+        sudo systemctl restart $service
+        echo "Service restarted"
+    fi
+    
+    echo "The listening port is: $old_port"
+
+    if [ "$old_port" != "$port" ]; then
+        echo "Current port is different. Changing to $port"
+        curl -i -X POST -d "json={\"listen_port\": $port}" "$api_base_url/setPreferences"
+        echo "Sent request to change port"
+        sudo systemctl restart $service
+        echo "Service restarted"
     fi
     sleep 45
 done
